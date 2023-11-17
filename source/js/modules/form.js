d3dd6c6e83ee3cb565d34b9fe09fb5b5cbcbd78b
Александр Головко <a-golovko2009@yandex.ru>
2023-11-17 21:31:07 +0500

    исправил оформление ошибки валидации

--- source/js/modules/form.js
+++ source/js/modules/form.js
@@ -1,18 +1,30 @@
 import Pristine from '../vendor/pristine';
 
-const VALID_NAME_SYMBOLS = /[a-zа-яё]/i;
-const VALID_NUMBER_SYMBOLS = /^[0-9]{1,}$/;
+const VALID_NAME_SYMBOLS = /^[a-zа-яё]{1,}$/i;
+const VALID_PHONE_SYMBOLS = /^[0-9]{1,}$/;
 const VALID_NAME_ERROR_MESSAGE = 'Введите имя кириллицей или латиницей';
-const VALID_NUMBER_ERROR_MESSAGE = 'Введите цифры номера без пробелов и спецсимволов';
+const VALID_PHONE_ERROR_MESSAGE = 'Введите цифры номера без пробелов и спецсимволов';
 
 const formElement = document.querySelector('.form');
+
 const inputNameElement = formElement.querySelector('.input[name=name]');
 const inputPhoneElement = formElement.querySelector('.input[name=phone]');
 
+const labelNameElement = inputNameElement.closest('label');
+const labelPhoneElement = inputPhoneElement.closest('label');
+
 const pristine = new Pristine(
-    formElement, {classTo: 'test', errorTextParent: 'test', errorTextClass: 'test-error'}, false);
+    formElement, {classTo: 'form__label', errorTextParent: 'form__label', errorTextClass: 'form__label--error'}, false);
 
 const hasValidName = (value) => {
+  const isValid = VALID_NAME_SYMBOLS.test(value);
+  if (!isValid) {
+    labelNameElement.classList.add('form__label--error');
+    labelNameElement.classList.remove('form__label--success');
+  } else {
+    labelNameElement.classList.remove('form__label--error');
+    labelNameElement.classList.add('form__label--success');
+  }
   return VALID_NAME_SYMBOLS.test(value);
 };
 
@@ -23,13 +35,21 @@ pristine.addValidator(
 );
 
 const hasValidPhone = (value) => {
-  return VALID_NUMBER_SYMBOLS.test(value);
+  const isValid = VALID_PHONE_SYMBOLS.test(value);
+  if (!isValid) {
+    labelPhoneElement.classList.add('form__label--error');
+    labelPhoneElement.classList.remove('form__label--success');
+  } else {
+    labelPhoneElement.classList.remove('form__label--error');
+    labelPhoneElement.classList.add('form__label--success');
+  }
+  return VALID_PHONE_SYMBOLS.test(value);
 };
 
 pristine.addValidator(
     inputPhoneElement,
     hasValidPhone,
-    VALID_NUMBER_ERROR_MESSAGE
+    VALID_PHONE_ERROR_MESSAGE
 );
 
 formElement.addEventListener('submit', (evt) => {
